f08ec5481a8a3b9a2a4a958191732a4e62b2c81b
reginafcompton <reginafcompton@gmail.com>
2016-06-12 21:58:18 +0200

    Start make_order method

--- lib/data_parser.rb
+++ lib/data_parser.rb
@@ -6,4 +6,14 @@ class DataParser
     @prices_array = args[:prices_array]
   end
 
+  def make_my_order(prices_array, total)
+    # Store all price possibilities in this.
+    price_possibilities = Array.new(total + 1) { [] }
+    # Store all totals in this.
+    totals = Array.new(total + 1) { 0 }
+
+    # Store all solutions in this.
+    all_solutions = Array.new(total + 1) { [0, []] }
+  end
+
 end
