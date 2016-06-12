8004be18d8398b4e1d96e831d0f685a6c1bbd1bf
reginafcompton <reginafcompton@gmail.com>
2016-06-12 21:37:20 +0200

    Create methods to find total and make a hash

---
+++ lib/DataRetriever.rb
@@ -0,0 +1,28 @@
+require 'nokogiri'
+require 'open-uri'
+require 'csv'
+require_relative 'variables.rb'
+
+class DataWriter
+
+  def self.write_file
+    doc = Nokogiri::HTML(open(URL))
+    File.write(CSV_FILE_PATH, doc.text)
+  end
+
+  def self.find_max_amount
+    max_amount = CSV.open(CSV_FILE_PATH, 'r') { |csv_line| csv_line.first[0][1..-1] }
+  end
+
+  def self.make_data_hash
+    menu_items_hash = Hash.new
+
+    CSV.foreach(CSV_FILE_PATH, :headers => true) do |csv_line|
+      # Add items to hash. Remove $.
+      menu_items_hash[csv_line[0]] = csv_line[1][1..-1]
+    end
+
+    menu_items_hash
+  end
+
+end
