915e58f3a51a9d3706cd6dd6144244dd7cccd8ef
reginafcompton <reginafcompton@gmail.com>
2016-06-14 14:28:45 +0200

    Create a make_integer method

--- lib/data_retriever.rb
+++ lib/data_retriever.rb
@@ -1,15 +1,18 @@
 class DataRetriever
 
-  def self.write_file
+  def write_file
     doc = Nokogiri::HTML(open(URL))
     File.write(CSV_FILE_PATH, doc.text)
   end
 
-  def self.find_max_amount
-    max_amount = CSV.open(CSV_FILE_PATH, 'r') { |csv_line| ((csv_line.first[0][1..-1].to_f) * 100).to_i }
+  def find_max_amount
+    max_amount = CSV.open(CSV_FILE_PATH, 'r') do |csv_line|
+      price = csv_line.first[0][1..-1]
+      make_integer(price)
+    end
   end
 
-  def self.make_data_hash
+  def make_data_hash
     menu_items_hash = Hash.new
 
     CSV.foreach(CSV_FILE_PATH, :headers => true) do |csv_line|
@@ -20,14 +23,19 @@ class DataRetriever
     menu_items_hash
   end
 
-  def self.make_data_array
+  def make_data_array
     menu_items_array = Array.new
 
     CSV.foreach(CSV_FILE_PATH, :headers => true) do |csv_line|
-      menu_items_array << ((csv_line[1][1..-1].to_f) * 100).to_i
+      price = csv_line[1][1..-1]
+      menu_items_array << make_integer(price)
     end
 
     menu_items_array.sort
+  end
+
+  def make_integer(string)
+    (string.to_f * 100).to_i
   end
 
 end
--- lib/foodsack_runner.rb
+++ lib/foodsack_runner.rb
@@ -5,9 +5,10 @@ require_relative '../config/variables.rb'
 require_relative 'data_parser.rb'
 require_relative 'data_retriever.rb'
 
-#DataRetriever.write_file
-target_total = DataRetriever.find_max_amount
-prices_array = DataRetriever.make_data_array
+retrieve_it = DataRetriever.new
+# retrieve_it.write_file
+target_total = retrieve_it.find_max_amount
+prices_array = retrieve_it.make_data_array
 
 parse_it = DataParser.new(total: target_total, prices_array: prices_array)
 p parse_it.make_my_order
